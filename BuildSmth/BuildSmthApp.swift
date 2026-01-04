41faa2e0b3426f96aca646e70d9dfaf507ca90e4
Joanne K <lyozzz017@gmail.com>
2026-01-04 15:50:36 +0100

    Initial Commit

---
+++ BuildSmth/BuildSmthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildSmthApp.swift
+//  BuildSmth
+//
+//  Created by Joanne Kuang on 2026/1/4.
+//
+
+import SwiftUI
+
+@main
+struct BuildSmthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
